6de20910de33a82563b0bb409f36abe7b1e3eb93
Admin <eaak2004@hotmail.com>
2019-03-16 17:37:29 +0000

    Add New Stuff

---
+++ DoubleTablesASPNETMVC5/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using DoubleTablesASPNETMVC5.Models.DB;
+using DoubleTablesASPNETMVC5.ViewModels.Home;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoubleTablesASPNETMVC5.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            StudentViewModels svm = new StudentViewModels();
+            List<Student> students = svm.GetAllStudents();
+
+            return View(students);
+        }
+    }
+}
